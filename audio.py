7238a996be2e276a5b621c9fb0d8403830450e30
rrolli <3Beccaboo@gmail.com>
2019-03-05 16:53:47 -0800

    Added audio.py file

---
+++ audio.py
@@ -0,0 +1,20 @@
+import serial
+import threading 
+from playsound import playsound
+
+arduino = serial.Serial('COM6', 9600, timeout=0.1)
+
+while True:
+    data = arduino.readline()
+    if data:
+        line = data.decode('utf-8')
+        print(line)
+        if line.startswith('Calibrate glove'):
+            key = input('')
+            key_byte = key.encode('utf-8')
+            print('Recieved key ')
+            print(key)
+            arduino.write(key_byte)
+        if line.startswith('letter = ') and line[-3].isupper():
+            letter = line[-3]
+            playsound('alphabet/%s.wav' % str(letter))
